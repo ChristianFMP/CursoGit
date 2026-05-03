f48d12da0c1124ee9a760e3680ec0753dd020e1d
chris <171565@unsaac.edu.pe>
2026-05-03 18:27:20 -0500

    Agregando los titulos a la resta y la multiplicacion

--- multiplicacion.py
+++ multiplicacion.py
@@ -1,3 +1,4 @@
 # Multiplicacion
 def multiplicacion(a,b):
+    print("Funcion para Multiplicar")
     return (a*b)
--- resta.py
+++ resta.py
@@ -1,3 +1,4 @@
 # Modulo para diferencia
 def resta(a,b):
+    print("Funcion para restar)
     return (a-b)
