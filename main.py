dcdf63f8b4cdf244d37d5ecc8889584ba5b9ee33
ChristianFMP <79106563+ChristianFMP@users.noreply.github.com>
2026-05-04 01:48:19 -0500

    Update main.py
    Agregando opcion por teclado a usuario para acceder a las funciones

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 print("Menu de Operaciones")
 print("Seleccione una Opcion")
+opcion = int(input("Ingrese su opcion: ")
 print("Opcion 1: Suma")
 print("Opcion 2: Resta")
 print("Opcion 3: Multiplicacion")
+
+if opcion == 1 :
+  print(suma(5,2))
+elif opcion == 2:
+  print(resta(5,2))
+else:
+  print(multiplicacion(5,2))
