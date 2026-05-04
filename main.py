3b5eee94462c3102b2f695d40cc6bf1e291a8567
chris <171565@unsaac.edu.pe>
2026-05-04 01:57:30 -0500

    Fin del proyecto

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from multiplicacion import multiplicacion
+from resta import resta
+from suma import suma
+
 print("Menu de Operaciones")
 print("Seleccione una Opcion")
-opcion = int(input("Ingrese su opcion: ")
+opcion = int(input("Ingrese su opcion: "))
 print("Opcion 1: Suma")
 print("Opcion 2: Resta")
 print("Opcion 3: Multiplicacion")
